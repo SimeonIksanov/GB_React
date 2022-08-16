54541ec36515c1e71ae30be4f2a9f21d0a906c10
Simeon Iksanov <simeon.iksanov@gmail.com>
2022-08-17 01:25:53 +0500

    React: HW_Lesson02 : add messageList state

--- Lesson02/app/src/App.js
+++ Lesson02/app/src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [messageList, setMessageList] = useState([]);
+  
   return (
     <div className="App">
       <header className="App-header">
